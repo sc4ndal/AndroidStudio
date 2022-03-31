0e5dc6305108716f9e6f1e99a9703e27bd462313
HaruAki <100898033+sc4ndal@users.noreply.github.com>
2022-03-31 19:52:09 +0900

    ListView

--- MainActivity.java
+++ MainActivity.java
@@ -1,66 +1,41 @@
-package com.example.callphone02;
+package com.example.listview;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.SearchManager;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button btnCall, btnGoogle, btnMap, btnSearch, btnMessage;
+    ListView LvFruit;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btnCall = (Button) findViewById(R.id.btnCall);
-        btnCall.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intentCall = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:01029231863"));
-                startActivity(intentCall);
-            }
-        });
-        btnGoogle = (Button) findViewById(R.id.btnGoogle);
-        btnGoogle.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intentGoogle = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.naver.com"));
-                startActivity(intentGoogle);
-            }
-        });
-        btnMap = (Button) findViewById(R.id.btnMap);
-        btnMap.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intentMap = new Intent(Intent.ACTION_VIEW, Uri.parse("geo:35.16634991634856, 129.07311173075632"));   //geo의 좌표로 지도 검색
-                startActivity(intentMap);
-            }
-        });
+        LvFruit = (ListView) findViewById(R.id.LvFruit);
+        //String[] data = {"사과", "바나나", "딸기", "블루베리", "포도", "파인애플", "드래곤후르츠", "키위", "수박", "메론", "망고", "산딸기", "아보카도", "레몬", "라임", "복숭아", "망고스틴"};
+        String[] data = {"01029231863","112","119"};
 
-        btnSearch = (Button) findViewById(R.id.btnSearch);
-        btnSearch.setOnClickListener(new View.OnClickListener() {
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, data);
+        LvFruit.setAdapter(adapter);
+
+        LvFruit.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
-            public void onClick(View view) {
-                Intent intentSearch = new Intent(Intent.ACTION_WEB_SEARCH);
-                intentSearch.putExtra(SearchManager.QUERY, "안드로이드");   //검색창에서 안드로이드 검색
-                startActivity(intentSearch);
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                String telNum = (String) adapterView.getItemAtPosition(i);
+                Toast.makeText(MainActivity.this, telNum, Toast.LENGTH_SHORT).show();
+//                Uri uri = Uri.parse("tel:"+telNum);
+                Intent call = new Intent(Intent.ACTION_DIAL, Uri.parse(("tel:"+telNum)));
+                startActivity(call);
             }
         });
-
-        btnMessage = (Button) findViewById(R.id.btnMessage);
-        btnMessage.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intentMessage = new Intent(Intent.ACTION_SENDTO, Uri.parse("smsto:01029231863"));    //smsto에 적혀있는 번호로 메세지 보냄
-                intentMessage.putExtra("sms_body", "ㅎㅇ");     //메세지 입력창에 ㅎㅇ라고 먼저 적어둠
-                startActivity(intentMessage);
-            }
-        });
-
     }
 }
